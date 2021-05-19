15597e4aebd6dec6ecdd57e917d203300c845aee
Lucas Bonanni <lucas.bonanni@thomsonreuters.com>
2021-05-19 20:23:38 -0300

    Add project list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { MainComponent } from './pages/main/main.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 const routes: Routes = [
   {path:'', component: MainComponent},
   {path:'about-me', component: AboutMeComponent},
+  {path:'projects', component: ProjectsComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './pages/main/main.component';
 import { SocialComponent } from './components/social/social.component';
 import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent,
     FooterComponent,
     NavbarComponent,
     MainComponent,
     SocialComponent,
     AboutMeComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
